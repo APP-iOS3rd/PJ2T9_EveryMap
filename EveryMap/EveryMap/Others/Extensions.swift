2be3e04d341c0940736c890f584bc4e8ced19c03
zxl3651 <zxl3651@naver.com>
2023-12-07 00:30:02 +0900

    [Feat] Others - Extension으로 Apikey 접근, ViewController - TMapView를 이용해 초기화면 지도 띄우기

---
+++ EveryMap/EveryMap/Others/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  EveryMap
+//
+//  Created by 이성현 on 2023/12/07.
+//
+
+import Foundation
+
+// APIKEY 를 가져오기 위한 Bundle extension
+extension Bundle {
+    var TmapApiKey : String? {
+        guard let file = self.path(forResource: "APIKEY-Info", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile: file),
+              let key = resource["Tmap-apikey"] as? String else { return nil }
+        return key
+    }
+    
+}
