d159b55f13060123a166564926b3c59fe5d4770e
zxl3651 <zxl3651@naver.com>
2023-12-07 12:46:42 +0900

    [Feat, Add] Main화면 TabBar 기능 추가, Views - MainView, SettingView 추가, Core Folder 추가, Others -> Utils 이름 변경

--- EveryMap/EveryMap/Views/HomeView.swift
+++ EveryMap/EveryMap/Views/HomeView.swift
@@ -1,23 +1,30 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  EveryMap
 //
-//  Created by 이성현 on 2023/12/06.
+//  Created by 이성현 on 2023/12/07.
 //
 
 import UIKit
 import TMapSDK
 
-class ViewController: UIViewController, TMapTapiDelegate {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class HomeView: UIViewController, TMapTapiDelegate {
+    
+    convenience init(title: String) {
+        self.init()
         
+        // 임시로 TMapView로 표현함
         let mapView = TMapView(frame: self.view.frame)
         self.view.addSubview(mapView)
         mapView.setApiKey(Bundle.main.TmapApiKey!)
-        
+        // title 있어도 되고 없어도 됨
+        self.title = title
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
     // Apikey 인증 성공 시 호출된다.
@@ -30,5 +37,5 @@ class ViewController: UIViewController, TMapTapiDelegate {
         print("APIKEY 인증 실패....")
     }
 
-}
 
+}
