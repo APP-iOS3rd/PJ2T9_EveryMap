1c4595234e64e711628c46bf9155dc92c8594c03
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-12-07 12:07:42 +0900

    [Add] Navermap-clientsecert, id 추가
    [Add] Navermap-clientsecert, id 추가

--- EveryMap/EveryMap/Others/Extensions.swift
+++ EveryMap/EveryMap/Others/Extensions.swift
@@ -21,5 +21,19 @@ extension Bundle {
               let resource = NSDictionary(contentsOfFile: file),
               let key = resource["Kakaomap-apikey"] as? String else { return nil }
         return key
+    }  
+    
+    var NavermapClientId : String? {
+        guard let file = self.path(forResource: "APIKEY-Info", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile: file),
+              let key = resource["Navermap-clientid"] as? String else { return nil }
+        return key
+    }
+    
+    var NavermapClientSecret : String? {
+        guard let file = self.path(forResource: "APIKEY-Info", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile: file),
+              let key = resource["Navermap-clientsecret"] as? String else { return nil }
+        return key
     }
 }
