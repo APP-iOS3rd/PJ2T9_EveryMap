43d56e820d356444e37e2bc031c9990e1c24917a
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-12-11 15:46:29 +0900

    [Add] View - SearchView, SearchResultView, ResultMapView, CompareView / Controller - SearchViewViewController, SearchResultViewViewController, ResultMapViewViewController, CompareViewViewController 추가
    [Add]
    View -
    SearchView,
    SearchResultView,
    ResultMapView,
    CompareView
    
    Controller - SearchViewViewController,
    SearchResultViewViewController,
    ResultMapViewViewController,
    CompareViewViewController
    
    추가

---
+++ EveryMap/EveryMap/Controllers/CompareViewViewController.swift
@@ -0,0 +1,8 @@
+//
+//  CompareViewViewController.swift
+//  EveryMap
+//
+//  Created by Taejun Ha on 12/11/23.
+//
+
+import Foundation
---
+++ EveryMap/EveryMap/Controllers/SearchResultViewViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResultViewViewController.swift
+//  EveryMap
+//
+//  Created by Taejun Ha on 12/11/23.
+//
+
+import Foundation
